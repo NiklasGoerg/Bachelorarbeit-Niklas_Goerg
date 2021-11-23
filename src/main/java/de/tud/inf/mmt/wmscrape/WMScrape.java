ae67a337017832f1fed8f58a240c58ee853c6b33
Felix Rülke <felix.ruelke@mailbox.tu-dresden.de>
2021-11-23 23:51:03 +0000

    added geckodriver for independent selenium use. mac+win+nux

--- src/main/java/de/tud/inf/mmt/wmscrape/WMScrape.java
+++ src/main/java/de/tud/inf/mmt/wmscrape/WMScrape.java
@@ -2,12 +2,28 @@ package de.tud.inf.mmt.wmscrape;
 
 import de.tud.inf.mmt.wmscrape.gui.GuiApplication;
 import javafx.application.Application;
+import org.springframework.beans.InvalidPropertyException;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class WMScrape {
 
     public static void main(String[] args) {
+
+        String OS = System.getProperty("os.name").toLowerCase(Locale.ENGLISH);
+        if ((OS.contains("mac")) || (OS.contains("darwin"))) {
+            System.setProperty("webdriver.gecko.driver","libs/geckodriver-mac64");
+        } else if (OS.contains("win")) {
+            System.setProperty("webdriver.gecko.driver","libs/geckodriver-win64.exe");
+        } else if (OS.contains("nux")) {
+            System.setProperty("webdriver.gecko.driver","libs/geckodriver-linux64");
+        } else {
+            throw new InvalidPropertyException(WMScrape.class,
+                    "webdriver.gecko.driver", "Für dieses Betriebssystem steht keine Geckodriver bereit.");
+        }
+
         Application.launch(GuiApplication.class, args);
     }
 }
--- src/main/java/de/tud/inf/mmt/wmscrape/springdata/SpringContextAccessor.java
+++ src/main/java/de/tud/inf/mmt/wmscrape/springdata/SpringContextAccessor.java
@@ -3,6 +3,7 @@ package de.tud.inf.mmt.wmscrape.springdata;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,15 +11,15 @@ public class SpringContextAccessor implements ApplicationContextAware {
 
     private static ApplicationContext context;
 
-    public static <T extends Object> T getBean(Class<T> beanClass) {
-        if (context != null) {
+    public static <T> T getBean(Class<T> beanClass) {
+        if (context != null && context.containsBean(String.valueOf(beanClass))) {
             return context.getBean(beanClass);
         }
         return null;
     }
 
     @Override
-    public void setApplicationContext(ApplicationContext context) throws BeansException {
-        this.context = context;
+    public void setApplicationContext(@NonNull ApplicationContext context) throws BeansException {
+        SpringContextAccessor.context = context;
     }
 }
