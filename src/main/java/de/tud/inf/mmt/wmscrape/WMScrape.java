99bf596a4704d6166ff23538f3737caa3a1235c4
FelixRTU <ruelke@protonmail.com>
2022-01-23 12:58:29 +0100

    forgot to include geckodriver

--- src/main/java/de/tud/inf/mmt/wmscrape/WMScrape.java
+++ src/main/java/de/tud/inf/mmt/wmscrape/WMScrape.java
@@ -22,11 +22,11 @@ public class WMScrape {
 
         String OS = System.getProperty("os.name").toLowerCase(Locale.ENGLISH);
         if ((OS.contains("mac")) || (OS.contains("darwin"))) {
-            System.setProperty("webdriver.gecko.driver","libs/geckodriver/mac_x86_64/geckodriver");
+            System.setProperty("webdriver.gecko.driver","src/main/resources/geckodriver/mac_x86_64/geckodriver");
         } else if (OS.contains("win")) {
-            System.setProperty("webdriver.gecko.driver","libs/geckodriver/win_64/geckodriver.exe");
+            System.setProperty("webdriver.gecko.driver","src/main/resources/geckodriver/win_64/geckodriver.exe");
         } else if (OS.contains("nux")) {
-            System.setProperty("webdriver.gecko.driver","libs/geckodriver/nux_64/geckodriver");
+            System.setProperty("webdriver.gecko.driver","src/main/resources/geckodriver/nux_64/geckodriver");
         } else {
             throw new InvalidPropertyException(WMScrape.class,
                     "webdriver.gecko.driver", "Für dieses Betriebssystem steht kein Geckodriver bereit.");
