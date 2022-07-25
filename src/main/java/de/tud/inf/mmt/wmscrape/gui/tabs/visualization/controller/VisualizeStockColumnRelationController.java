c6db7d879ce74d600dc44f05dd8e58601c242f8d
jonashoen <hoenisch.jonas@gmail.com>
2022-07-25 18:01:01 +0200

    down show invalid columns

--- src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/visualization/controller/VisualizeStockColumnRelationController.java
+++ src/main/java/de/tud/inf/mmt/wmscrape/gui/tabs/visualization/controller/VisualizeStockColumnRelationController.java
@@ -3,6 +3,7 @@ package de.tud.inf.mmt.wmscrape.gui.tabs.visualization.controller;
 import de.tud.inf.mmt.wmscrape.dynamicdb.course.CourseColumnRepository;
 import de.tud.inf.mmt.wmscrape.dynamicdb.transaction.TransactionColumnRepository;
 import de.tud.inf.mmt.wmscrape.dynamicdb.watchlist.WatchListColumnRepository;
+import de.tud.inf.mmt.wmscrape.gui.tabs.PrimaryTabManager;
 import de.tud.inf.mmt.wmscrape.helper.PropertiesHelper;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
@@ -64,24 +65,47 @@ public class VisualizeStockColumnRelationController {
                 watchListTableAmountColumn
         );
 
-        for (var column : courseColumnRepository.findAll()) {
+        var courseColumns = courseColumnRepository.findAll();
+        for (var column : courseColumns) {
             courseDropDown.getItems().add(column.getName());
         }
-        courseDropDown.getSelectionModel().select(columnNames.get(stockCourseTableCourseColumn));
 
-        for (var column : transactionColumnRepository.findAll()) {
+        var selectedCourseColumn = columnNames.get(stockCourseTableCourseColumn);
+        if(courseColumns.stream().anyMatch(c -> c.getName().equals(selectedCourseColumn))) {
+            courseDropDown.getSelectionModel().select(selectedCourseColumn);
+        }
+
+        var transactionColumns = transactionColumnRepository.findAll();
+        for (var column : transactionColumns) {
             transactionAmountDropDown.getItems().add(column.getName());
         }
-        transactionAmountDropDown.getSelectionModel().select(columnNames.get(transactionTableAmountColumn));
 
-        for (var column : watchListColumnRepository.findAll()) {
+        var selectedTransactionAmountColumn = columnNames.get(transactionTableAmountColumn);
+        if(transactionColumns.stream().anyMatch(c -> c.getName().equals(selectedTransactionAmountColumn))) {
+            transactionAmountDropDown.getSelectionModel().select(selectedTransactionAmountColumn);
+        }
+
+        var watchListColumns = watchListColumnRepository.findAll();
+        for (var column : watchListColumns) {
             watchListAmountDropDown.getItems().add(column.getName());
             watchListBuyCourseDropDown.getItems().add(column.getName());
             watchListSellCourseDropDown.getItems().add(column.getName());
         }
-        watchListAmountDropDown.getSelectionModel().select(columnNames.get(watchListTableAmountColumn));
-        watchListBuyCourseDropDown.getSelectionModel().select(columnNames.get(watchListTableBuyCourseColumn));
-        watchListSellCourseDropDown.getSelectionModel().select(columnNames.get(watchListTableSellCourseColumn));
+
+        var selectedWatchListTableAmountColumn = columnNames.get(watchListTableAmountColumn);
+        if(watchListColumns.stream().anyMatch(c -> c.getName().equals(selectedWatchListTableAmountColumn))) {
+            watchListAmountDropDown.getSelectionModel().select(selectedWatchListTableAmountColumn);
+        }
+
+        var selectedWatchListTableBuyCourseColumn = columnNames.get(watchListTableBuyCourseColumn);
+        if(watchListColumns.stream().anyMatch(c -> c.getName().equals(selectedWatchListTableBuyCourseColumn))) {
+            watchListBuyCourseDropDown.getSelectionModel().select(selectedWatchListTableBuyCourseColumn);
+        }
+
+        var selectedWatchListTableSellCourseColumn = columnNames.get(watchListTableSellCourseColumn);
+        if(watchListColumns.stream().anyMatch(c -> c.getName().equals(selectedWatchListTableSellCourseColumn))) {
+            watchListSellCourseDropDown.getSelectionModel().select(selectedWatchListTableSellCourseColumn);
+        }
     }
 
     /**
@@ -89,19 +113,57 @@ public class VisualizeStockColumnRelationController {
      */
     @FXML
     public void saveConfiguration() {
-        PropertiesHelper.setProperty(stockCourseTableCourseColumn, courseDropDown.getSelectionModel().selectedItemProperty().getValue());
-        PropertiesHelper.setProperty(transactionTableAmountColumn, transactionAmountDropDown.getSelectionModel().selectedItemProperty().getValue());
-        PropertiesHelper.setProperty(watchListTableBuyCourseColumn, watchListBuyCourseDropDown.getSelectionModel().selectedItemProperty().getValue());
-        PropertiesHelper.setProperty(watchListTableSellCourseColumn, watchListSellCourseDropDown.getSelectionModel().selectedItemProperty().getValue());
-        PropertiesHelper.setProperty(watchListTableAmountColumn, watchListAmountDropDown.getSelectionModel().selectedItemProperty().getValue());
+        var isInputValid = true;
 
-        Alert alert = new Alert(Alert.AlertType.INFORMATION,"Spaltenzuordnung gespeichert", ButtonType.OK);
+        var selectedStockCourseTableCourseColumn = courseDropDown.getSelectionModel().selectedItemProperty().getValue();
+        if(selectedStockCourseTableCourseColumn != null) {
+            PropertiesHelper.setProperty(stockCourseTableCourseColumn, selectedStockCourseTableCourseColumn);
+        } else {
+            PrimaryTabManager.decorateField(courseDropDown, "Dieses Feld darf nicht leer sein!", false, true);
+            isInputValid = false;
+        }
+
+        var selectedTransactionTableAmountColumn = transactionAmountDropDown.getSelectionModel().selectedItemProperty().getValue();
+        if(selectedTransactionTableAmountColumn != null) {
+            PropertiesHelper.setProperty(transactionTableAmountColumn, selectedTransactionTableAmountColumn);
+        } else {
+            PrimaryTabManager.decorateField(transactionAmountDropDown, "Dieses Feld darf nicht leer sein!", false, true);
+            isInputValid = false;
+        }
+
+        var selectedWatchListTableBuyCourseColumn = watchListBuyCourseDropDown.getSelectionModel().selectedItemProperty().getValue();
+        if(selectedWatchListTableBuyCourseColumn != null) {
+            PropertiesHelper.setProperty(watchListTableBuyCourseColumn, selectedWatchListTableBuyCourseColumn);
+        } else {
+            PrimaryTabManager.decorateField(watchListBuyCourseDropDown, "Dieses Feld darf nicht leer sein!", false, true);
+            isInputValid = false;
+        }
+
+        var selectedWatchListTableSellCourseColumn = watchListSellCourseDropDown.getSelectionModel().selectedItemProperty().getValue();
+        if(selectedWatchListTableSellCourseColumn != null) {
+            PropertiesHelper.setProperty(watchListTableSellCourseColumn, selectedWatchListTableSellCourseColumn);
+        } else {
+            PrimaryTabManager.decorateField(watchListSellCourseDropDown, "Dieses Feld darf nicht leer sein!", false, true);
+            isInputValid = false;
+        }
+
+        var selectedWatchListTableAmountColumn = watchListAmountDropDown.getSelectionModel().selectedItemProperty().getValue();
+        if(selectedWatchListTableAmountColumn != null) {
+            PropertiesHelper.setProperty(watchListTableAmountColumn, selectedWatchListTableAmountColumn);
+        } else {
+            PrimaryTabManager.decorateField(watchListAmountDropDown, "Dieses Feld darf nicht leer sein!", false, true);
+            isInputValid = false;
+        }
+
+        Alert alert = new Alert(Alert.AlertType.INFORMATION,"Die ausgewählten Spaltenzuordnung wurden gespeichert", ButtonType.OK);
         var window = courseDropDown.getScene().getWindow();
         alert.setX(window.getX()+(window.getWidth()/2)-200);
         alert.setY(window.getY()+(window.getHeight()/2)-200);
 
         alert.showAndWait();
 
-        window.hide();
+        if(isInputValid) {
+            window.hide();
+        }
     }
 }
